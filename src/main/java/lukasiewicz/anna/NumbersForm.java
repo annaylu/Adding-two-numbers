fdad7aa12211ea4dcc1de390d59d6252f0f8d574
annaylu <64437394+annaylu@users.noreply.github.com>
2020-07-28 02:06:46 +0200

    Update NumbersForm.java

--- src/main/java/lukasiewicz/anna/NumbersForm.java
+++ src/main/java/lukasiewicz/anna/NumbersForm.java
@@ -6,11 +6,11 @@ import javax.validation.constraints.NotNull;
 public class NumbersForm {
 	
 	@NotNull
-	@Digits(integer=309,fraction=1001)
+	@Digits(integer=50,fraction=50)
 	private Double num1;
 	
 	@NotNull
-	@Digits(integer=309,fraction=1000)
+	@Digits(integer=50,fraction=50)
 	private Double num2;
 
 	public Double getNum1() {
